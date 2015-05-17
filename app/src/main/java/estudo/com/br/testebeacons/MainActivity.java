4e55b6a082f9ddf9ee10ea8a3d093604765ac0be
NatanXimenes <natanximenes29@gmail.com>
2015-05-17 18:22:10 -0300

    Teste

---
+++ app/src/main/java/estudo/com/br/testebeacons/MainActivity.java
@@ -0,0 +1,17 @@
+package estudo.com.br.testebeacons;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
